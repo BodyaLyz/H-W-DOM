a8e5c240c40912b304dc8db671d48715dfe78284
Bodya Lyzen <43245185+BodyaLyz@users.noreply.github.com>
2018-12-05 23:00:53 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 var divbox = [];
 
-window.onload = function blockCreation() {
-    for (var i = 0; i <= 19; i++) {  
+window.onload = function createBlocks() {
+    for (var i = 0; i < 20 ; i++) {  
         divbox[i] = document.createElement('div');
-        divbox[i].style.backgroundColor = randomColor();
+        divbox[i].style.backgroundColor = getRandomColor();
         divbox[i].innerText = i+1;
-        divbox[i].style.color = randomColor();
+        divbox[i].style.color = getRandomColor();
         document.body.append(divbox[i]);
     }
 
 }
 
-function randomColor() {
+function getRandomColor() {
     let r = Math.floor(Math.random() * 256);
     let g = Math.floor(Math.random() * 256);
     let b = Math.floor(Math.random() * 256);
@@ -20,7 +20,7 @@ function randomColor() {
 
 function changeColor() {
     for (i = 0; i < divbox.length; i++) {
-        divbox[i].style.backgroundColor = randomColor();
-        divbox[i].style.color = randomColor();
+        divbox[i].style.backgroundColor = getRandomColor();
+        divbox[i].style.color = getRandomColor();
     }
 }
